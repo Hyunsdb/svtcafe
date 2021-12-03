c72c72070576aad0594a6b404a9e396ba066fa9a
hyuna <hyunsdb@naver.com>
2021-12-03 23:53:14 +0900

    Initial commit

---
+++ src/main/java/com/hyuns/svtcafe/SvtcafeApplication.java
@@ -0,0 +1,13 @@
+package com.hyuns.svtcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvtcafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvtcafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyuns/svtcafe/SvtcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyuns.svtcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvtcafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
