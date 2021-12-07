128d97bcfe5982cf2d3ec744008d5d3b953c327c
hyuna <hyunsdb@naver.com>
2021-12-08 01:08:40 +0900

    feat: 카페 목록(메인화면)

--- src/main/java/com/hyuns/svtcafe/controller/MainController.java
+++ src/main/java/com/hyuns/svtcafe/controller/MainController.java
@@ -1,12 +1,24 @@
 package com.hyuns.svtcafe.controller;
 
+import com.hyuns.svtcafe.entity.Cafe;
+import com.hyuns.svtcafe.repository.CafeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+
+    private final CafeRepository cafeRepository;
+
     @GetMapping("/")
-    public String mainPage(){
-        return "index";
+    public String mainPage(Model model) {
+        model.addAttribute("cafes", cafeRepository.findAll());
+
+        return "main";
     }
 }
