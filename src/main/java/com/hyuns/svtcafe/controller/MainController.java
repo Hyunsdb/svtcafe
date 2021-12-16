797c1066bcf057aae09916c4f4313aec62e1d492
hyuna <hyunsdb@naver.com>
2021-12-16 17:42:55 +0900

    feat: Read post
    html 링크 수정

--- src/main/java/com/hyuns/svtcafe/controller/MainController.java
+++ src/main/java/com/hyuns/svtcafe/controller/MainController.java
@@ -2,18 +2,14 @@ package com.hyuns.svtcafe.controller;
 
 import com.hyuns.svtcafe.dto.CafeSearchDto;
 import com.hyuns.svtcafe.entity.Cafe;
-import com.hyuns.svtcafe.repository.CafeRepository;
 import com.hyuns.svtcafe.service.CafeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
